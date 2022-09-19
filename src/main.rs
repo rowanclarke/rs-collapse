dfe5ade2a27aeb20022e31e6cb04f2e6e6952efb
Rowan Clarke <r.n.clarke@lancaster.ac.uk>
2022-09-19 13:31:20 +0100

    Added number of trues in BigBoolean

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,29 @@
 use std::iter::Filter;
 use std::ops::Range;
 
-pub struct BigBoolean<const N: usize>([u8; N / 8 + 1])
+pub struct BigBoolean<const N: usize>
 where
-    [u8; N / 8 + 1]: Sized;
+    [u8; N / 8 + 1]: Sized,
+{
+    bools: [u8; N / 8 + 1],
+    trues: usize,
+}
 
 impl<const N: usize> BigBoolean<N>
 where
     [u8; N / 8 + 1]: Sized,
 {
     pub fn fill(b: bool) -> Self {
-        BigBoolean(
-            [match b {
+        Self {
+            bools: [match b {
                 true => 0xFF,
                 false => 0x00,
             }; N / 8 + 1],
-        )
+            trues: match b {
+                true => N,
+                false => 0,
+            },
+        }
     }
 }
 
@@ -28,11 +36,11 @@ where
     type IntoIter = Filter<Range<usize>, impl FnMut(&usize) -> bool>;
 
     fn into_iter(self) -> Self::IntoIter {
-        (0..N).filter(|&x| 1 << (x % 8) & self.0[x / 8] != 0)
+        (0..N).filter(|&x| 1 << (x % 8) & self.bools[x / 8] != 0)
     }
 }
 
 fn main() {
     let b = BigBoolean::<7>::fill(true);
-    println!("{:?}", b.into_iter().collect::<Vec<usize>>());
+    println!("{} {:?}", b.trues, b.into_iter().collect::<Vec<usize>>());
 }
