cccbbebb40a3fe6b890535c6d8a0a1ee516710e5
Rowan Clarke <r.n.clarke@lancaster.ac.uk>
2022-12-27 00:08:17 +0000

    Implementation of Sudoku

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,29 @@
-#![feature(type_alias_impl_trait, generic_const_exprs)]
-use std::iter::Filter;
-use std::ops::Range;
+#![feature(
+    type_alias_impl_trait,
+    return_position_impl_trait_in_trait,
+    impl_trait_projections
+)]
 
-pub struct BigBoolean<const N: usize>
-where
-    [u8; N / 8 + 1]: Sized,
-{
-    bools: [u8; N / 8 + 1],
-    trues: usize,
-}
+mod collapse;
+mod iter;
+mod sudoku;
 
-impl<const N: usize> BigBoolean<N>
-where
-    [u8; N / 8 + 1]: Sized,
-{
-    pub fn fill(b: bool) -> Self {
-        Self {
-            bools: [match b {
-                true => 0xFF,
-                false => 0x00,
-            }; N / 8 + 1],
-            trues: match b {
-                true => N,
-                false => 0,
-            },
-        }
-    }
-}
-
-impl<'a, const N: usize> IntoIterator for &'a BigBoolean<N>
-where
-    [u8; N / 8 + 1]: Sized,
-{
-    type Item = usize;
-    type IntoIter = Filter<Range<usize>, impl FnMut(&usize) -> bool>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        (0..N).filter(|&x| 1 << (x % 8) & self.bools[x / 8] != 0)
-    }
-}
+use collapse::Collapse;
+use iter::AccumulateFilter;
+use std::rc::Rc;
+use sudoku::Solver;
 
 fn main() {
-    let b = BigBoolean::<7>::fill(true);
-    println!("{} {:?}", b.trues, b.into_iter().collect::<Vec<usize>>());
+    let mut s = Solver::new();
+    let a = s.solve();
+    let mut b = [[0; 9]; 9];
+    a.unwrap()
+        .into_iter()
+        .for_each(|(coord, i)| b[coord.0][coord.1] = i);
+    for i in 0..9 {
+        for j in 0..9 {
+            print!("{} ", b[i][j]);
+        }
+        println!();
+    }
 }
