98e136d36ef11be82ec700cad5f9b32c0d2ffcbe
Rowan Clarke <r.n.clarke@lancaster.ac.uk>
2023-01-04 13:43:32 +0000

    As Library

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![feature(
+    type_alias_impl_trait,
+    return_position_impl_trait_in_trait,
+    impl_trait_projections
+)]
+
+mod collapse;
+mod grid;
+mod iter;
+mod puzzle;
+mod sudoku;
--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,21 @@
     impl_trait_projections
 )]
 
+/*
 mod collapse;
+mod grid;
 mod iter;
+mod puzzle;
 mod sudoku;
 
+use puzzle::{Solver, Puzzle};
 use collapse::{Collapse, FromCollapse};
-use iter::AccumulateFilter;
-use sudoku::{Solver, Sudoku};
 
 fn main() {
     let mut solver = Solver::new();
-    let initial = solver.get_state([
-        [0, 0, 0, 0, 0, 4, 0, 5, 2],
-        [6, 0, 0, 0, 0, 0, 0, 0, 0],
-        [9, 0, 5, 0, 2, 0, 0, 3, 0],
-        [0, 0, 0, 8, 0, 0, 7, 0, 0],
-        [2, 0, 3, 0, 4, 0, 0, 9, 0],
-        [0, 1, 0, 0, 0, 0, 0, 0, 0],
-        [5, 0, 1, 0, 0, 7, 9, 0, 0],
-        [0, 6, 0, 0, 5, 0, 0, 0, 0],
-        [0, 4, 0, 0, 0, 0, 0, 1, 0],
-    ]);
-    let solution = solver.solve(Some(initial)).unwrap();
-    println!("{}", Sudoku::from_collapse(solution));
+    let solution = solver.solve(None).unwrap();
+    println!("{}", Puzzle::from_collapse(solution));
 }
+*/
+
+fn main() {}
